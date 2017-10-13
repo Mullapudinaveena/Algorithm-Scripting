d7e1bdf394c73fa2c156a3ed31725fecba33b971
Naveena_m <16699169+Mullapudinaveena@users.noreply.github.com>
2017-10-13 19:16:58 +0530

    basic algorithms

---
+++ repeatString.js
@@ -0,0 +1,23 @@
+//Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+
+//repeatStringNumTimes("*", 3) should return "***".
+//repeatStringNumTimes("abc", 3) should return "abcabcabc".
+//repeatStringNumTimes("abc", 4) should return "abcabcabcabc".
+//repeatStringNumTimes("abc", 1) should return "abc".
+//repeatStringNumTimes("*", 8) should return "********".
+//repeatStringNumTimes("abc", -2) should return "".
+
+//solution - 1
+function repeatStringNumTimes(str, num) {
+  if(num > 0){
+    return str.repeat(num);
+  }
+  return "";
+}
+
+repeatStringNumTimes("abc", 3);
+
+//solution - 2
+function repeatStringNumTimes(str, num){
+	return num ? 0 : str.repeat(num) : "";
+}

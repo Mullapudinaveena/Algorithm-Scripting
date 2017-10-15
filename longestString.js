78e13d3de500e07c767abe96926f69816e5ce080
Naveena_m <16699169+Mullapudinaveena@users.noreply.github.com>
2017-10-15 21:58:35 +0530

    basic algorithms

---
+++ longestString.js
@@ -0,0 +1,32 @@
+//Return the length of the longest word in the provided sentence.
+
+//Your response should be a number.
+//findLongestWord("The quick brown fox jumped over the lazy dog") should return a number.
+//findLongestWord("The quick brown fox jumped over the lazy dog") should return 6.
+//findLongestWord("May the force be with you") should return 5.
+//findLongestWord("Google do a barrel roll") should return 6.
+//findLongestWord("What is the average airspeed velocity of an unladen swallow") should return 8.
+//findLongestWord("What if we try a super-long word such as otorhinolaryngology") should return 19.
+
+function findLongestWord(str) {
+  
+  //1.Split the sentence into array of strings.
+  var words  = str.split(' ');
+  
+  //2.Initalize maxLength to carry the max length.
+  var maxLength = 0;
+  
+  //3.For loop
+  for(var i = 0; i < words.length; i++){
+	  
+	//Check if the length of the string is greater than maxlength.  
+    if(words[i].length > maxLength){
+       maxLength = words[i].length;
+    }
+  }
+  
+  //4.return the longest word.
+  return maxLength;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");

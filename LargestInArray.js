73357440a1c8bb56e756e67897ed2fa04b9f883b
Naveena_m <16699169+Mullapudinaveena@users.noreply.github.com>
2017-10-15 22:05:33 +0530

    basic algorithms

---
+++ LargestInArray.js
@@ -0,0 +1,22 @@
+//Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+/* largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return an array.
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return [27,5,39,1001].
+largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]) should return [9, 35, 97, 1000000]. */
+
+function largestOfFour(arr) {
+  
+  var largestArr = [0,0,0,0];
+
+  for(var i = 0; i < arr.length; i++){
+    for(var j = 0; j < arr[i].length; j++){
+      if(arr[i][j] > largestArr[i]){
+        largestArr[i] = arr[i][j];
+      }
+    }
+  }
+
+  return largestArr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
